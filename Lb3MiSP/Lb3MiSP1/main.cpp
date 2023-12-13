1718b49b9d78169e03f0c3a0cae46ec1eab4b925
KirillZhiganov3276 <145041753+KirillZhiganov3276@users.noreply.github.com>
2023-12-13 18:40:38 +0300

    Add files via upload

---
+++ Lb3MiSP/Lb3MiSP1/main.cpp
@@ -0,0 +1,111 @@
+#include <UnitTest++/UnitTest++.h>
+#include "modAlphaCipher.h"
+#include <iostream>
+#include <locale>
+#include <codecvt>
+
+using namespace std;
+SUITE(KeyTest) //макрос для теста ключа
+{
+
+    TEST(LargeLetters) {
+        modAlphaCipher test(L"ПРИВЕТ"); //проверка на верхний регистр
+        CHECK(true);
+    }
+    TEST(LargeAndSmallLetters) {
+        modAlphaCipher test(L"НОВЫЙгод"); //проверка на верхний и нижний регистр
+        CHECK(true);
+    }
+    TEST(EmptyKey) {
+        CHECK_THROW(modAlphaCipher test(L""), cipher_error); //проверка на отсутствие ключа негативного сценария макроса THROW
+    }
+    TEST(Key_with_a_space) {
+        CHECK_THROW(modAlphaCipher test(L"как дела"), cipher_error); //проверка ключа содержащей пробел
+    }
+    TEST(Key_with_invalid_characters_number) {
+        CHECK_THROW(modAlphaCipher test(L"10дней"), cipher_error); //проверка ключа содежащей цифры
+    }
+    TEST(Key_with_invalid_characters_special_character) {
+        CHECK_THROW(modAlphaCipher test(L"Правда?"), cipher_error); //проверка ключа содержащей специальные символы
+    }
+}
+
+struct KeyAB_fixture {
+    modAlphaCipher * pointer;
+    KeyAB_fixture()
+    {
+        pointer = new modAlphaCipher(L"АВ");
+    }
+    ~KeyAB_fixture()
+    {
+        delete pointer;
+    }
+};
+
+SUITE(EncryptTest) //макрос проверки закодированных слов
+{
+    TEST_FIXTURE(KeyAB_fixture, LargeLetters) {
+        wstring open_text = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //исходный текст
+        wstring result_correct = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ"; //ожидаемый результат
+        CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text)); //возвращать true если ожидаемый результат равен указателю на закодированный 
+                                                                          //исходный текст
+    }
+    TEST_FIXTURE(KeyAB_fixture, SmallLetters) {
+        wstring open_text = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        wstring result_correct = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ";
+        CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text));
+    }
+    TEST_FIXTURE(KeyAB_fixture,NumberInText ) {
+        wstring open_text = L"добрый1234де нь";
+        CHECK_THROW(pointer->encrypt(open_text),cipher_error);
+    }
+    TEST_FIXTURE(KeyAB_fixture,TextWithSpecialSymbol) {
+        wstring open_text = L"при/вет";
+        CHECK_THROW(pointer->encrypt(open_text),cipher_error);//cipher_error возвращает какой тип ошибки был совершён
+    }
+    TEST_FIXTURE(KeyAB_fixture,TextWithASpace ) {
+        wstring open_text = L"алло алло";
+        CHECK_THROW(pointer->encrypt(open_text),cipher_error);
+    }
+
+}
+SUITE(DecryptTest)
+{
+   TEST_FIXTURE(KeyAB_fixture, LargeLetters) {
+        wstring cipher_text = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ";
+        wstring result_correct = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));//если  результат раскодирования равен ожидаемому результату, то возвращается true
+    }
+    TEST_FIXTURE(KeyAB_fixture, Smallletters) {
+        wstring cipher_text = L"агведжёизкймлонрптсфуцхшчъщьыюэая";
+        wstring result_correct = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));
+    }
+    
+    TEST_FIXTURE(KeyAB_fixture, EmptyText) {
+        wstring cipher_text = L"33попугая";
+        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
+    }
+    TEST_FIXTURE(KeyAB_fixture,LargeAndSmallLetters ) {
+        wstring cipher_text = L"ЗДраВстуЙТЕ";
+        wstring result_correct = L"ЗВРЮВПТСЙРЕ";
+        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));
+    }
+
+    TEST_FIXTURE(KeyAB_fixture,TextWithNumber) {
+        wstring cipher_text = L"Ну как";
+        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
+    }
+    TEST_FIXTURE(KeyAB_fixture,TextWithSymbol) {
+        wstring cipher_text = L"при/вет";
+        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
+    }
+    TEST_FIXTURE(KeyAB_fixture,TextWithASpace ) {
+        wstring cipher_text = L"при вет";
+        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
+    }
+}
+int main()
+{
+    return UnitTest::RunAllTests();
+}
